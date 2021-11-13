de9091fe13f01d41b0ea9d87c02b9cc1beedd3f8
PorciusFestus <93726885+PorciusFestus@users.noreply.github.com>
2021-11-13 17:19:32 -0500

    Added organizational comments.

--- Project 3/MonteCarlo.py
+++ Project 3/MonteCarlo.py
@@ -5,15 +5,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# *************** Main MonteCarlo Algorithm ****************
+# 1) and 2) are irrelevant to the code, being the assignment and problem statements respectively
 
-# At this point, if you haven't started on this yet, there is still nothing stopping us from using
-# either python or java for this, so whichever you prefer. If you use Java, we can just import
-# data from a .csv file for analysis, etc. That's what I did for project 2 using R for analysis
-# instead of python (python does most everything R can do though, they are comparable for
-# analysis purposes)
-
-# 3) Model Analysis
+# 3) MODEL ANALYSIS
 # OBJECTIVE 1: Gain an understanding of th drop error model.
 
 tau = 57                            # Standard Deviation of drop point in inches
@@ -21,7 +15,7 @@ a = 1/tau                           # Scale Parameter
 
 x = np.linspace(0, 350, 1000)       # Axes Values
 
-# 1.1: Graph of f_X
+# 3.1) Graph of f_X
 
 pdf = a**2 * x * np.e**(-.5*a**2 * x**2)
 plt.plot(x, pdf, label='f_X (x)')  # Plot some data on the (implicit) axes.
@@ -31,9 +25,10 @@ plt.xlim(0, x.max()*1.1)
 plt.ylim(0, pdf.max()*1.1)
 plt.title("PDF of X")
 plt.legend()
+plt.savefig("pdf_X.png", bbox_inches='tight')
 plt.show()
 
-# 1.2 Graph of F_X
+# 3.2) Graph of F_X
 
 cdf = 1 - np.e**(-.5*a**2 * x**2)
 plt.plot(x, cdf, label='F_X (x)')  # Plot some data on the (implicit) axes.
@@ -43,10 +38,10 @@ plt.xlim(0, x.max()*1.1)
 plt.ylim(0, 1)
 plt.title("CDF of X")
 plt.legend()
+plt.savefig("CDF_X.png", bbox_inches='tight')
 plt.show()
 
-
-# 1.3 Graph of Three Circles
+# 3.3) Graph of Three Circles
 
 p = np.array([0.5, 0.7, 0.9])                   # the probabilities that will give the radii
 x_p = np.sqrt(-2 * np.log(-1*p + 1) / a ** 2)   # the radii for the three circles
@@ -82,9 +77,46 @@ plt.title('Landing Areas with Respective Probabilities', fontsize=12)
 plt.xlim(-175, 175)
 plt.ylim(-250, 150)
 plt.grid(linestyle='--')
-plt.savefig("three_circles.png", bbox_inches='tight')
 plt.legend()
+plt.savefig("three_circles.png", bbox_inches='tight')
 plt.show()
 
 
+# 4) LAWS OF LARGE NUMBERS
 
+# OBJECTIVE 2: Demonstrate empirically the convergence of the sample mean to the population mean,
+# when the sample size increases without bound
+
+# 4.2) Experiment
+
+# 4.2.1) Design a Monte-Carlo simulation algorithm
+# TODO: Bruce
+
+# 4.2.2) Simulate the outcomes of many deliveries of newspaper via a drone by generating
+# independent realizations of X
+
+# 4.2.3) Calculate 110 independent estimates of the sample mean for the seven sample sizes
+
+# 4.2.4) Make a graph of all the estimates
+
+# 4.2.5,6,7 may not require any coding, but if they do, they can go here.
+
+
+# 5) CENTRAL LIMIT THEOREM
+
+# OBJECTIVE 3: Demonstrate empirically the convergence of Z_n to Z in distribution.
+
+# 5.2) Experiment
+
+# 5.2.1) Prepare samples for the analyses.
+
+# 5.2.2) Perform Analyses [the substeps will be within a loop
+
+
+
+# 5.3) Summarize Results [might require further graphing and calculations, might not]
+# NOTE: LaTeX will read from a .csv file, so we probably want the code to output tabular results
+# as .csv files which we can then read directly instead of typing out the tables by hand,
+# which can be somewhat tedious, especially for large tables.
+
+# 5.4) and 5.5) should be strictly report.
