7e8ca81a0ee544c18fb07922fcc7cde9d4cc4526
PorciusFestus <93726885+PorciusFestus@users.noreply.github.com>
2021-11-17 17:46:22 -0500

    4.2.2,3,4 coded, untested. 5.1, 5.2.1,2,3,4 coded, untested.

--- Project 3/MonteCarlo.py
+++ Project 3/MonteCarlo.py
@@ -4,25 +4,27 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from statistics import NormalDist
+import pandas as pd
 
 # 1) and 2) are irrelevant to the code, being the assignment and problem statements respectively
 
 # 3) MODEL ANALYSIS
-# OBJECTIVE 1: Gain an understanding of th drop error model.
+# OBJECTIVE 1: Gain an understanding of the drop error model.
 
-tau = 57                            # Standard Deviation of drop point in inches
-a = 1/tau                           # Scale Parameter
+tau = 57  # Standard Deviation of drop point in inches
+a = 1 / tau  # Scale Parameter
 
-x = np.linspace(0, 350, 1000)       # Axes Values
+x = np.linspace(0, 350, 1000)  # Axes Values
 
 # 3.1) Graph of f_X
 
-pdf = a**2 * x * np.e**(-.5*a**2 * x**2)
+pdf = a ** 2 * x * np.e ** (-.5 * a ** 2 * x ** 2)
 plt.plot(x, pdf, label='f_X (x)')  # Plot some data on the (implicit) axes.
 plt.xlabel('Drop Distance x')
 plt.ylabel('Probability Density')
-plt.xlim(0, x.max()*1.1)
-plt.ylim(0, pdf.max()*1.1)
+plt.xlim(0, x.max() * 1.1)
+plt.ylim(0, pdf.max() * 1.1)
 plt.title("PDF of X")
 plt.legend()
 plt.savefig("pdf_X.png", bbox_inches='tight')
@@ -30,11 +32,11 @@ plt.show()
 
 # 3.2) Graph of F_X
 
-cdf = 1 - np.e**(-.5*a**2 * x**2)
+cdf = 1 - np.e ** (-.5 * a ** 2 * x ** 2)
 plt.plot(x, cdf, label='F_X (x)')  # Plot some data on the (implicit) axes.
 plt.xlabel('Drop Distance x')
 plt.ylabel('Cumulative Probability')
-plt.xlim(0, x.max()*1.1)
+plt.xlim(0, x.max() * 1.1)
 plt.ylim(0, 1)
 plt.title("CDF of X")
 plt.legend()
@@ -43,8 +45,8 @@ plt.show()
 
 # 3.3) Graph of Three Circles
 
-p = np.array([0.5, 0.7, 0.9])                   # the probabilities that will give the radii
-x_p = np.sqrt(-2 * np.log(-1*p + 1) / a ** 2)   # the radii for the three circles
+p = np.array([0.5, 0.7, 0.9])  # the probabilities that will give the radii
+x_p = np.sqrt(-2 * np.log(-1 * p + 1) / a ** 2)  # the radii for the three circles
 
 t = [0, 0]
 print('The values of x_p are:')
@@ -52,16 +54,16 @@ print('for p = ' + str(p[0]) + ', x_p = ' + str(x_p[0]) + ' inches')
 print('for p = ' + str(p[1]) + ', x_p = ' + str(x_p[1]) + ' inches')
 print('for p = ' + str(p[2]) + ', x_p = ' + str(x_p[2]) + ' inches')
 
-theta = np.linspace(0, 2*np.pi, 100)
+theta = np.linspace(0, 2 * np.pi, 100)
 
-c1_x = x_p[0]*np.cos(theta)
-c1_y = x_p[0]*np.sin(theta)
+c1_x = x_p[0] * np.cos(theta)
+c1_y = x_p[0] * np.sin(theta)
 
-c2_x = x_p[1]*np.cos(theta)
-c2_y = x_p[1]*np.sin(theta)
+c2_x = x_p[1] * np.cos(theta)
+c2_y = x_p[1] * np.sin(theta)
 
-c3_x = x_p[2]*np.cos(theta)
-c3_y = x_p[2]*np.sin(theta)
+c3_x = x_p[2] * np.cos(theta)
+c3_y = x_p[2] * np.sin(theta)
 
 fig, ax = plt.subplots(1)
 
@@ -92,12 +94,49 @@ plt.show()
 # 4.2.1) Design a Monte-Carlo simulation algorithm
 # TODO: Bruce
 
+def monte_carlo(*args):
+    pass
+
+
 # 4.2.2) Simulate the outcomes of many deliveries of newspaper via a drone by generating
 # independent realizations of X
 
 # 4.2.3) Calculate 110 independent estimates of the sample mean for the seven sample sizes
 
 # 4.2.4) Make a graph of all the estimates
+
+n_vec1 = [10, 30, 50, 100, 250, 500, 1000]
+abscissa1 = []
+ordinate1 = []
+
+
+def sample_mean(count):
+    x_vec = []
+    for i in range(1, count):
+        x_vec.append(monte_carlo(i))
+
+    return sum(x_vec) / count
+
+
+for n in n_vec1:
+    for index in range(1, 110):
+        abscissa1.append(n)
+        ordinate1.append(sample_mean(n))
+
+mu_x = 1/a * np.sqrt(np.pi/2)
+
+ax.plot(abscissa1, ordinate1, label='M_n')
+ax.plot(abscissa1, mu_x, label='Population Mean')
+
+plt.xlabel('Sample Size (n)')
+plt.ylabel('Sample Mean (M_n)')
+plt.title('Landing Areas with Respective Probabilities', fontsize=12)
+# plt.xlim(-175, 175)
+# plt.ylim(-250, 150)
+plt.grid(linestyle='--')
+plt.legend()
+plt.savefig("M_n.png", bbox_inches='tight')
+plt.show()
 
 # 4.2.5,6,7 may not require any coding, but if they do, they can go here.
 
@@ -108,9 +147,82 @@ plt.show()
 
 # 5.2) Experiment
 
-# 5.2.1) Prepare samples for the analyses.
+# 5.2.1) Prepare samples for the analyses and
+# 5.2.2) Perform Analyses
 
-# 5.2.2) Perform Analyses [the substeps will be within a loop
+n_vec2 = [5, 10, 15, 30]
+K = 550
+k_vec = range(1, K)
+
+phi_abscissa = np.linspace(-2.5, 2.5, K)
+phi = []
+for phi_i in phi_abscissa:
+    phi.append(NormalDist().cdf(phi_i))
+
+for n in n_vec2:
+    abscissa2 = []
+    ordinate2 = []
+    m_n = []
+    z_n = []
+    F_n = []
+    MAD = 0
+
+    # Prepare samples (5.2.1)
+    for k in k_vec:
+        m_n.append(sample_mean(n))
+
+    # 5.2.2.1 calculate the estimates of the mean and variance of M_n
+    mean = sum(m_n)/K
+    variance = sum(np.subtract(np.power(m_n, 2), mean**2)) / K
+
+    # 5.2.2.2 transform the sample of M_n into a sample of the standardized random variable Z_n
+    z_n = np.divide(np.subtract(m_n, mean), variance)
+    z_n_sorted = sorted(z_n)
+
+    # 5.2.2.3 estimate from the sample of Z_n the probabilities of seven events
+    z_j = [-1.4, -1.0, -0.5, 0, 1.0, 1.4]
+
+    for j in range(0, 6) :
+        # find percentage of z_n values less than or equal to z_j
+        counter = 0
+        for z in z_n_sorted:
+            if z <= z_j[j]:
+                counter += 1
+            else:
+                break
+
+        F_n.append(counter/K)
+
+        # 5.2.2.4 Evaluate the goodness-of-fit using the MAD
+        MAD_j = abs(F_n[j] - NormalDist.cdf(z_j[j]))
+
+        if MAD_j > MAD:
+            MAD = MAD_j
+
+    # 5.2.2.5 Draw a figure showing:
+
+    # i) the seven points {(z_j, F_n(z_j)) : j = 1, ... ,7}
+    ax.plot(z_j, F_n, label='F_n(z_j)')
+
+    # ii) The standard normal cdf phi
+    ax.plot(phi_abscissa, phi, label='Phi(z)')
+
+    # iii) The MAD as a highlighted interval of probability (ordinate at point z_j at which it
+    # occurs (abscissa)
+    ax.plot(abscissa1, ordinate1, label='M_n')
+
+    # And make it look nice :)
+    plt.xlabel('Z')
+    plt.ylabel('Probability')
+    plt.title('Landing Areas with Respective Probabilities', fontsize=12)
+    # plt.xlim(-175, 175)
+    # plt.ylim(-250, 150)
+    plt.grid(linestyle='--')
+    plt.legend()
+    plt.savefig("M_n.png", bbox_inches='tight')
+    plt.show()
+
+
 
 
 
